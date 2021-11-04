33ea3021290698370ad1d4146333e1e7317c6147
Mohamed Elssaffy <ma7amad123410000@gmail.com>
2021-11-04 04:31:33 +0200

    console

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
 export const endpoint = `http://localhost:3000/api/graphql`;
 export const prodEndpoint = process.env.NEXT_PUBLIC_BACKEND_URL;
 export const perPage = 2;
