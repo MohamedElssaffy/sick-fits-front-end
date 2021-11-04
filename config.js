ca5a4c8ab9b8d2378a633c2d43865c70430fc4c9
Mohamed Elssaffy <ma7amad123410000@gmail.com>
2021-11-04 04:11:01 +0200

    add dotenv

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-// This is client side config only - don't put anything in here that shouldn't be public!
+import 'dotenv/config';
+
 export const endpoint = `http://localhost:3000/api/graphql`;
 export const prodEndpoint = process.env.BACKEND_URL;
 export const perPage = 2;
