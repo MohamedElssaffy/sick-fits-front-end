6c8767587a0cbd260590bf8146d5f9befaac0c77
Mohamed Elssaffy <ma7amad123410000@gmail.com>
2021-11-04 04:18:46 +0200

    modified env

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-import 'dotenv/config';
-
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = process.env.BACKEND_URL;
+export const prodEndpoint = process.env.NEXT_PUBLIC_BACKEND_URL;
 export const perPage = 2;
